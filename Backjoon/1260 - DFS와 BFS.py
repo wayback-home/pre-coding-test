360be57b740d57ba604af4ed7f3db31860ebf7b1
wayback-home <dudtj4329@gmail.com>
2022-06-30 14:42:08 +0900

    fixed untracked files

--- Backjoon/1260 - DFS와 BFS.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-node, line, startnode = sys.stdin.readline().strip().split(" ")
--- Backjoon/2739 - 구구단.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-getNum = int(sys.stdin.readline().strip())
-
-for i in range(1, 10, 1):
-    print(getNum, "*", i, "=", getNum * i)
