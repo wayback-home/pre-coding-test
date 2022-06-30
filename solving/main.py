77c377cf05d59e3da9c5d27f5bc1794094689b8e
wayback-home <dudtj4329@gmail.com>
2022-06-30 14:40:56 +0900

    add gitignore

--- solving/main.py
+++ solving/main.py
@@ -1,21 +1,60 @@
 import sys
+from collections import deque
 
-getLines = int(sys.stdin.readline())
 
-for i in range(0, getLines, 1):
-    checkBracket = 0
+def push(list, x):
+    list.append(x)
 
-    getBracket = list(sys.stdin.readline().strip())
 
-    for j in range(0, len(getBracket), 1):
-        if getBracket[j] == "(":
-            checkBracket += 1
-        elif getBracket[j] == ")":
-            checkBracket -= 1
-            if checkBracket < 0:
-                break
-
-    if not checkBracket:
-        print("YES")
+def pop(list):
+    if list:
+        popList = list.popleft()
+        print(popList, end="")
     else:
-        print("NO")
+        print(-1)
+
+
+def size(list):
+    print(len(list))
+
+
+def empty(list):
+    if not list:
+        print(1)
+    else:
+        print(0)
+
+
+def front(list):
+    if not list:
+        print(-1)
+    else:
+        print(list[0], end="")
+
+
+def back(list):
+    if not list:
+        print(-1)
+    else:
+        print(list[-1], end="")
+
+
+intList = deque([])
+
+getCommandLines = int(sys.stdin.readline())
+for _ in range(0, getCommandLines, 1):
+    getCommand = sys.stdin.readline()
+
+    if "push" in getCommand:
+        getCommand, getNum = getCommand.split(" ")
+        push(intList, getNum)
+    elif "pop" in getCommand:
+        pop(intList)
+    elif "size" in getCommand:
+        size(intList)
+    elif "empty" in getCommand:
+        empty(intList)
+    elif "front" in getCommand:
+        front(intList)
+    elif "back" in getCommand:
+        back(intList)
