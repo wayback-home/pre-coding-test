557a7f5d8b6f257bac51fe53115e5933c3a572c0
wayback-home <dudtj4329@gmail.com>
2022-07-29 00:35:06 +0900

    예제 정답, 시간초과

--- Backjoon/22942 - 데이터 체커.py
+++ Backjoon/22942 - 데이터 체커.py
@@ -10,11 +10,11 @@ for _ in range(N):
     x[_], r[_] = map(int, sys.stdin.readline().strip().split(" "))
 
 for i in range(N - 1):
-    for j in range(1, N - i):
-        d = abs(x[i] - x[i + j])
-        if d > r[i] + r[i + j]:
+    for j in range(i + 1, N):
+        d = abs(x[i] - x[j])
+        if d > r[i] + r[j]:
             continue
-        elif d < abs(r[i] - r[i + j]):
+        elif d < abs(r[i] - r[j]):
             continue
         elif not d:
             continue
